c313d2c53443d73ae315ca0682e790cba6707296
Yiyeh <YiyehDev@gmail.com>
2023-04-16 12:56:02 -0400

    aplicacion terminada

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+import 'package:yes_no_app/presentation/screens/chat_screeen.dart';
+
 
 void main() => runApp(const MyApp());
 
@@ -7,15 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider() )
+      ],
+      child: MaterialApp(
+        title: 'Yes No App',
+        theme: AppTheme( selectedColor: 0 ).theme(),
+        debugShowCheckedModeBanner: false,
+        home: const ChatScreen()
       ),
     );
   }
---
+++ lib/presentation/screens/chat_screeen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/chat/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/chat/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/chat/widgets/shared/message_field_box.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
+
+class ChatScreen extends StatelessWidget {
+  final imageUrl =
+      'https://th.bing.com/th/id/OIP.8W60tIm5KeDchZ_a1VyHxAAAAA?pid=ImgDet&rs=1';
+
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(imageUrl),
+          ),
+        ),
+        title: const Text('Mi amor'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  controller: chatProvider.chatScrollController,
+                  itemCount: chatProvider.messageList.length,
+                  itemBuilder: (context, index) {
+                    final message = chatProvider.messageList[index];
+
+                    return (message.fromWho == FromWho.hers)
+                        ? HerMessageBubble( message: message,)
+                        : MyMessageBubble( message: message,);
+                  }),
+            ),
+
+            //caja de texto
+             MessageFieldBox(
+              onValue: chatProvider.sendMessage ,
+             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
